c4303c2476a06dc4c741771be30d889d2343e0f9
gybran nauval <nauvalgybran@gmail.com>
2024-08-20 17:05:37 +0700

    create widget input berat

---
+++ lib/components/widgets/widget_input_berat.dart
@@ -0,0 +1,72 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ongkir_indonesia/controllers/home/home_controller.dart';
+import 'package:get/get.dart';
+
+class BuildInputBerat extends GetView<HomeController> {
+  const BuildInputBerat({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            autocorrect: false,
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            controller: controller.beratController,
+            decoration: const InputDecoration(
+                labelText: "Berat Barang",
+                hintText: "Masukkan Berat Barang",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)))),
+            onChanged: (value) {
+              // print(value);
+              controller.ubahBerat(value);
+            },
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        SizedBox(
+          width: 150,
+          child: DropdownSearch<String>(
+            popupProps: const PopupProps.bottomSheet(
+              showSelectedItems: true,
+            ),
+            items: const [
+              "ton",
+              "kwintal",
+              "ons",
+              "lbs",
+              "pound",
+              "kg",
+              "hg",
+              "dag",
+              "gram",
+              "dg",
+              "cg",
+              "mg"
+            ],
+            dropdownDecoratorProps: const DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                labelText: "Satuan Berats",
+                hintText: "country in menu mode",
+              ),
+            ),
+            onChanged: (value) {
+              controller.ubahSatuan(value!);
+              // print(value);
+            },
+            selectedItem: "gram",
+          ),
+        )
+      ],
+    );
+  }
+}
